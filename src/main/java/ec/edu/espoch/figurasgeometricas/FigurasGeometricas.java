d5aeb665fe4f4eaa4c846846aaf674c2f3080456
JIrleidySantos <jirleidysantosm@gmail.com>
2024-11-26 12:02:50 -0500

    Creacion del proyecto

---
+++ src/main/java/ec/edu/espoch/figurasgeometricas/FigurasGeometricas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.figurasgeometricas;
+
+/**
+ *
+ * @author mundo
+ */
+public class FigurasGeometricas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
---
+++ src/main/java/ec/edu/espoch/figurasgeometricas/clases/Cuadrado.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author mundo
+ */
+public class Cuadrado {
+
+    public double lado;
+    public double area;
+    public double perimetro;
+
+    public Cuadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public void imprimir() {
+        System.out.println("El lado del Cuadrado es: " + lado);
+    }
+
+    public double calcularArea() {
+        return (lado * lado);
+    }
+
+    public double calcularPerimetro() {
+        return (lado + lado + lado + lado);
+    }
+}
