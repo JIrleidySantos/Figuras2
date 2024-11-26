83f8c197559770b8546d0b4cfbd1920597dd053a
JIrleidySantos <jirleidysantosm@gmail.com>
2024-11-26 12:06:38 -0500

    Creacion de las clases circulo y cuaadrado

--- src/main/java/ec/edu/espoch/figurasgeometricas/FigurasGeometricas.java
+++ src/main/java/ec/edu/espoch/figurasgeometricas/FigurasGeometricas.java
@@ -4,6 +4,9 @@
 
 package ec.edu.espoch.figurasgeometricas;
 
+
+
+
 /**
  *
  * @author mundo
@@ -11,7 +14,26 @@ package ec.edu.espoch.figurasgeometricas;
 public class FigurasGeometricas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Circulo objCirculo = new Circulo(10.6);
+
+        objCirculo.imprimir();
+
+        double area = objCirculo.calcularArea();
+        System.out.println("El Area del circulo es: " + area);
+
+        double perimetro = objCirculo.calcularPerimetro();
+        System.out.println("El perimetro del Circulo es " + perimetro);
+
+        Cuadrado objCuadrado = new Cuadrado(30.5);
+
+        objCuadrado.imprimir();
+
+        double areaC = objCuadrado.calcularArea();
+        System.out.println("El Area del cuadrado es: " + areaC);
+
+        double perimetroC = objCuadrado.calcularPerimetro();
+        System.out.println("El perimetro del Cuadrado es: " + perimetroC);
+
         
     }
 }
